4dd8d4f983dd52f555c7483f5b280c21aa8e5839
Seantheshe3p <seanwiddowson95@gmai.com>
2025-11-18 10:49:30 -0600

    analzer

---
+++ analyzer.py
@@ -0,0 +1,26 @@
+import pytest
+import test_analyzer
+
+def count_words(text):
+    for each in text:
+        i = i + 1
+    return i
+def count_chars(text):
+    if text == "":
+        return 0
+    for each in text:
+         for char in each:
+            i = i + 1
+    return i
+def find_most_common_word(text):
+    words = text.split
+    if not words:
+        return None
+    word_counts = Counter(words)
+    most_common_word, _ =  word_counts.most_common(1)[0]
+    return most_common_word
+
+
+
+if __name__ == "__main__":
+    print(find_most_common_word("The imaginary fox fox"))
